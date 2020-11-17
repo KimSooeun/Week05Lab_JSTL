eb5e9dfea3388139b7a750f4f774bdb5b9fda836
817949 <817949@SAIT224420IT.ACDM.DS.SAIT.CA>
2020-11-16 20:39:33 -0700

    Finished Lab5

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ShoppingListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        
+        if ( action != null && action.equals("logout") ) { 
+            session.invalidate();
+            response.sendRedirect("ShoppingList");
+        }
+        else if ( session.getAttribute("username") == null ) {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+        }    
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        
+        HttpSession session = request.getSession();
+        
+        if ( action.equals("register") && !request.getParameter("username").equals("") ) {
+            session.setAttribute("username", request.getParameter("username"));
+            session.setAttribute("items", new ArrayList<String>());
+        } else if ( action.equals("add") ) {
+            ArrayList<String> items = (ArrayList<String>) session.getAttribute("items");
+            items.add(request.getParameter("newItem"));
+            session.setAttribute("items", items);
+        } else if ( action.equals("delete") ) {
+            String item = request.getParameter("item");
+            if ( item != null ) {
+                ArrayList<String> items = (ArrayList<String>) session.getAttribute("items");
+                items.remove(item);
+                session.setAttribute("items", items);
+            }
+        }
+        
+        response.sendRedirect("ShoppingList");       
+    }
+}
---
+++ web/WEB-INF/register.jsp
@@ -0,0 +1,17 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Shopping List</title>
+    </head>
+    <body>
+        <h1>Shopping List</h1>
+        <form action="" method="post">
+            Username: 
+            <input type="text" name="username" value="">
+            <input type="submit" value="Register name">
+            <input type="hidden" name="action" value="register">
+        </form>
+    </body>
+</html>
